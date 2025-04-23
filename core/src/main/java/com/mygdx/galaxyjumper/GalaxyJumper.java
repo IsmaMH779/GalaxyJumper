996eb04f8af514c20d838e140be2e6c310d2fb2b
ismael <ismaelmh08@gmail.com>
2025-04-23 17:49:36 +0200

    Initial commit

---
+++ core/src/main/java/com/mygdx/galaxyjumper/GalaxyJumper.java
@@ -0,0 +1,23 @@
+package com.mygdx.galaxyjumper;
+
+import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.utils.ScreenUtils;
+
+/** {@link com.badlogic.gdx.ApplicationListener} implementation shared by all platforms. */
+public class GalaxyJumper extends Game {
+    private SpriteBatch batch;
+    private Texture image;
+
+    @Override
+    public void create() {
+        batch = new SpriteBatch();
+        image = new Texture("libgdx.png");
+        setScreen(new GameScreen());
+    }
+
+}
